f9c923cc2fad138f237b065429eae041f549fee9
Elias <elias.koller38@gmail.com>
2018-06-06 14:28:54 +0200

    fixed rotation (really)

--- src/mygame/AudioManager.java
+++ src/mygame/AudioManager.java
@@ -15,27 +15,42 @@ import com.jme3.scene.Node;
  * @author koller
  */
 public class AudioManager {
-    
+
     private AssetManager am;
     private AudioNode gun;
-    
-    public AudioManager (AssetManager am) {
+    private AudioNode horsetechno;
+
+    public AudioManager(AssetManager am) {
         this.am = am;
         init();
     }
-    
+
     private void init() {
         gun = new AudioNode(am, "Sounds/Gun2.wav", AudioData.DataType.Buffer);
         gun.setPositional(false);
         gun.setLooping(false);
         gun.setVolume(2);
+
+        horsetechno = new AudioNode(am, "Sounds/HorseTechno.wav", AudioData.DataType.Buffer);
+        horsetechno.setPositional(false);
+        horsetechno.setLooping(false);
+        horsetechno.setVolume(2);
     }
-    
+
     public void attachToRoot(Node root) {
         root.attachChild(gun);
+        root.attachChild(horsetechno);
     }
-    
+
     public void playGunSound() {
         gun.playInstance();
+    }
+
+    public void playHorseTechno() {
+        horsetechno.play();
+    }
+
+    public void stopHorseTechno() {
+        horsetechno.stop();
     }
 }
